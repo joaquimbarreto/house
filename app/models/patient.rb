3a5a23d2375c1265501c1dcfc2bfcd52535424aa
Metahaem <tom.rowson@gmail.com>
2019-01-03 18:27:47 +0000

    gem

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,9 +3,8 @@ class Patient < ApplicationRecord
   has_many :suffers
   has_many :symptoms, through: :suffers
 
-  
+
     has_secure_password
     validates :name, presence: true, uniqueness: true
     validates :password, presence: true
   end
-end
